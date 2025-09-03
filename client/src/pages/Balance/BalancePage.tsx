3a3758eed74e67adb4de4aab4478210a725834d9
TechSavvyAce <152211544+TechSavvyAce@users.noreply.github.com>
2025-09-02 21:48:15 -0400

    second

--- client/src/pages/Balance/BalancePage.tsx
+++ client/src/pages/Balance/BalancePage.tsx
@@ -215,7 +215,7 @@ const BalancePage: React.FC = () => {
                       />
                       <Title level={4}>安全支付</Title>
                       <Text type="secondary" style={{ display: "block", marginBottom: "16px" }}>
-                        使用onetimeping.eu进行安全的加密货币支付
+                        使用 onetimeping.eu 进行安全的加密货币支付
                       </Text>
                     </div>
                   </Col>
